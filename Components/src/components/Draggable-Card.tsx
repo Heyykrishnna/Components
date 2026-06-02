ce62f206a6c2916889fdab5fa09952c3854cac74
Yatharth <khandelwalyatharth39@gmail.com>
2026-06-02 15:36:35 +0530

    updated

--- Components/src/components/Draggable-Card.tsx
+++ Components/src/components/Draggable-Card.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { motion } from 'motion/react'
+import { motion, useMotionValue, useTransform } from 'motion/react'
 
 
 const Initial_Stack = [
@@ -39,16 +39,32 @@ const DraggableCard = () => {
     )
 }
 
+const StackSpring = { type: "spring" as const, stiffness:380, damping: 32}
+
 
 const StackCard = ({item, index, total, onSendToBack}: {item: {title: string, description: string, src: string}, index: number, total: number, onSendToBack: () => void}) => {
+    const x = useMotionValue(0);
+    const rotate = useTransform(x, [-150, 150], [-12, 12])
+    const isTop = index === 0;
+
     return (
         <motion.div 
+        drag={isTop ? 'x' : false}
+        dragConstraints={{
+            left: -150,
+            right: 150
+        }}
+        dragElastic={0.1}
         style={{
             zIndex: total - index,
+            rotate,
+            x
         }}
         animate={{
             y: `${- index * 5}%`,
+            scale: 1 - index * 0.05,
         }}
+        transition={StackSpring}
         className="absolute inset-0">
             <img src={item.src} alt={item.title} className="pointer-events-none h-full min-h-96 w-full select-none rounded-xl object-cover"/>
             <h2 className="absolute bottom-8 left-4 font-bold text-xl text-white z-20"> 
