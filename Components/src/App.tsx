f1538ed1e959542c6024da1b188e900da7988388
Yatharth <khandelwalyatharth39@gmail.com>
2026-06-02 15:20:28 +0530

    up

--- Components/src/App.tsx
+++ Components/src/App.tsx
@@ -1,10 +1,11 @@
 // import HeroSection from './components/Hero-Section'
-import GooeyEffect from './components/Gooey-Effect'
+// import GooeyEffect from './components/Gooey-Effect'
+import DraggableCard from './components/Draggable-Card'
 
 const App = () => {
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
-      <GooeyEffect />
+      <DraggableCard />
     </div>
   )
 }
---
+++ Components/src/components/Draggable-Card.tsx
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+import { motion } from 'motion/react'
+
+
+const Initial_Stack = [
+    {
+        title: "Switzerland",
+        description: "Mountains, lakes, and chocolate.",
+        src: "https://images.unsplash.com/photo-1570161766218-f8488ebb8078?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        title: "Italy",
+        description: "Food, culture, and history.",
+        src: "https://images.unsplash.com/photo-1780042426982-cb794203ea1d?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        title: "France",
+        description: "Art, fashion, and wine.",
+        src: "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?q=80&w=2240&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        title: "Germany",
+        description: "Food, culture, and history.",
+        src: "https://images.unsplash.com/photo-1622214366189-72b19cc61597?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        title: "USA",
+        description: "Art, fashion, and wine.",
+        src: "https://images.unsplash.com/photo-1508433957232-3107f5fd5995?q=80&w=2372&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+]
+
+const DraggableCard = () => {
+    const [stack, setStack] = useState(Initial_Stack)
+    return (
+        <div className="relative flex h-96 w-80 items-center justify-center">
+            {stack.map((item, index) => <StackCard key={item.title} item={item} index={index} total={stack.length} onSendToBack={() => { }} /> )}
+        </div>
+    )
+}
+
+
+const StackCard = ({item, index, total, onSendToBack}: {item: {title: string, description: string, src: string}, index: number, total: number, onSendToBack: () => void}) => {
+    return (
+        <motion.div 
+        style={{
+            zIndex: total - index,
+        }}
+        animate={{
+            y: `${- index * 5}%`,
+        }}
+        className="absolute inset-0">
+            <img src={item.src} alt={item.title} className="pointer-events-none h-full min-h-96 w-full select-none rounded-xl object-cover"/>
+            <h2 className="absolute bottom-8 left-4 font-bold text-xl text-white z-20"> 
+                {" "}
+                {item.title} 
+            </h2>
+            <p className="absolute bottom-4 left-4 font-medium text-sm text-white/60 z-20"> 
+                {" "}
+                {item.description} 
+            </p>
+            <div className="absolute inset-0 h-full w-full bg-black/50 mask-t-from-50% rounded-xl"/>
+        </motion.div>
+    )
+}
+
+export default DraggableCard 
