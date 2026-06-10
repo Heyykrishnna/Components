c7f9cbcdd0807905204fbf37fd2010c3d23044ba
Yatharth <khandelwalyatharth39@gmail.com>
2026-06-10 23:04:02 +0530

    teams

--- Components/src/App.tsx
+++ Components/src/App.tsx
@@ -1,10 +1,11 @@
 // import HeroSection from './components/Hero-Section'
 // import GooeyEffect from './components/Gooey-Effect'
 // import DraggableCard from './components/Draggable-Card'
-import WhisperedFlow from './components/Whispered-Flow'
+// import WhisperedFlow from './components/Whispered-Flow'
+import Teams from './components/Teams'
 
 const App = () => {
-  return <WhisperedFlow />
+  return <Teams />
 }
 
 export default App
---
+++ Components/src/components/Teams.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Teams() {
+  return (
+    <div className='relative flex min-h-screen items-center justify-center'>
+        Teams
+    </div>
+  )
+}
